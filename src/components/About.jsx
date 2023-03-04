8f96abe90662eba86a22a1e8c239caeaca1adfd1
thanhtd <thanhtd@vnext.vn>
2023-03-04 23:50:51 +0700

    push code

---
+++ src/components/About.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div
+      name="about"
+      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+    >
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+            About Me
+          </p>
+        </div>
+
+        <p className="text-xl">
+          I would like to become a good developer and great
+          mentor!
+        </p>
+        <br />
+        <p className="text-xl">
+          My short-term goal is to be apprenticed and
+          procient in successfully completing all assigned
+          tasks.
+        </p>
+        <p className="text-xl">
+          My long-term goal is to become a leader and become
+          a manager for a mini-project.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
